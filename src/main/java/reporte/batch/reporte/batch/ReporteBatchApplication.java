4a3152b026cbd8cb5eeeb023a2b572fa893b37df
DarioMarmolejo <xxdausmalo@gmail.com>
2023-01-16 14:47:30 -0600

    CREATE - creación de reporte batch inicial

---
+++ src/main/java/reporte/batch/reporte/batch/ReporteBatchApplication.java
@@ -0,0 +1,13 @@
+package reporte.batch.reporte.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reporte/batch/reporte/batch/ReporteBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package reporte.batch.reporte.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
